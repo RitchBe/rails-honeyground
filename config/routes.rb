25d6ba96debd55ef1509017d04334a46ffbb0d0c
Richard Houet <itchy.ooc@gmail.com>
2017-11-13 14:21:05 +0000

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => 'accounts'
   root to: 'pages#home'
+  resources :users  do
+    resources :products
+    resources :sales , only: [:new, :create, :index, :show]
+  end
 
+
+  # resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
