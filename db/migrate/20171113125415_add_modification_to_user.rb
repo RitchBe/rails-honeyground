4d984e3fc7874148c94506ee898a97d8b9ee9c44
Richard Houet <itchy.ooc@gmail.com>
2017-11-13 13:55:37 +0000

    modifie model of user to have name nickname address and type

---
+++ db/migrate/20171113125415_add_modification_to_user.rb
@@ -0,0 +1,8 @@
+class AddModificationToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :nickname, :string
+    add_column :users, :address, :string
+    add_column :users, :type, :string
+  end
+end
