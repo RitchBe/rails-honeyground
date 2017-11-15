94b163c7def1b31c2e32b24ab7b5ea6109b54d23
Diego S. Mezquiriz <diegomezquiriz@gmail.com>
2017-11-15 16:08:00 +0000

    maps styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :nickname, presence: true
   validates :address, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
   # validates :category, presence: true, inclusion: {in: USERCATEGORY}
   has_many :sales
   has_many :products
