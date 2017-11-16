361e8669e8c8c535c22e708ff969658d92ac26bd
Diego S. Mezquiriz <diegomezquiriz@gmail.com>
2017-11-16 11:41:39 +0000

    fixed gmaps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,6 @@ class UsersController < ApplicationController
 
   def index
     @sellers = User.where(category: 'Seller')
-
-    @sellers = User.where.not(latitude: nil, longitude: nil)
-
-    @hash = Gmaps4rails.build_markers(@sellers) do |user, marker|
-      marker.lat user.latitude
-      marker.lng user.longitude
-    end
   end
 
 
